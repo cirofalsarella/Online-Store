82b7f1b0fef7960267536464644cf3fd8d7c835b
Ciro Falsarella <cirofalsarella@gmail.com>
2023-05-31 13:53:27 -0300

    Feat: display option for the header

--- App/src/App.js
+++ App/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
+
 import {Header} from "./Components"
 
+const headerContext = createContext();
+
 function App() {
+  const [headerDisplay, setHeaderDisplay] = useState(true)
+
   return (
     <ChakraProvider >
-      <Header />
-      <Outlet />
+      {headerDisplay && <Header />}
+
+      <headerContext.Provider value={setHeaderDisplay}>
+        <Outlet />
+      </headerContext.Provider>
+
     </ChakraProvider>
   );
 }
 
+export {
+  headerContext
+}
 export default App;
--- App/src/pages/web/login/login.js
+++ App/src/pages/web/login/login.js
@@ -1,4 +1,10 @@
+import { headerContext } from "../../../App"
+import { useContext } from "react";
+
 const Login = () => {
+  const setHeaderDisplay = useContext(headerContext)
+  setHeaderDisplay(false)
+  
   return (
     <div>Login</div>
     );
