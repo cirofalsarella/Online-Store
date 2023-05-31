d1a2af901a43f631f0bea60680a8d0ee682ce951
Ciro Falsarella <cirofalsarella@gmail.com>
2023-05-31 15:15:57 -0300

    Feat: Login page structure

--- App/src/pages/web/login/login.js
+++ App/src/pages/web/login/login.js
@@ -1,12 +1,75 @@
 import { headerContext } from "../../../App"
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { Card, Flex, Image, Heading, Input, Button, Text } from "@chakra-ui/react";
+
+import ImagemLogo from "../../../assets/logo.png"
+
+const delay = ms => new Promise(
+  resolve => setTimeout(resolve, ms)
+  );
+  
 
 const Login = () => {
+  const navigate = useNavigate();
+  
   const setHeaderDisplay = useContext(headerContext)
   setHeaderDisplay(false)
-  
+
+  const [isInvalid, setInvalid] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const tryLogin = async event => {
+    setLoading(true)
+    await delay(1000);
+    
+    if (email !== "admin" || password !== "admin")  setInvalid(true)
+    else  return navigate("/")
+    
+    setLoading(false)
+  }
+
   return (
-    <div>Login</div>
+    <Flex justify={"center"} align={"center"} height="100vh">
+      <Card
+        height={"70vh"} width={"40vw"}
+        direction={"column"} justify={"space-around"}
+      >
+        <Flex align={"center"} justify={"center"} gap={8}>
+          <Image src={ImagemLogo} objectFit={'cover'} boxSize={"10vh"}/>
+          <Heading padding={"10px"}>CIV</Heading>
+        </Flex>
+
+
+        <Flex direction={"column"} gap={10} margin={"20px"}>
+          <Input 
+            isInvalid={isInvalid} errorBorderColor='crimson' placeholder='E-mail'
+            value={email} onChange={ e => setEmail(e.target.value) }
+          />
+          
+          <Input 
+            isInvalid={isInvalid} errorBorderColor='crimson' placeholder='Password'
+            value={password} onChange={ e => setPassword(e.target.value) }
+            />
+
+          <Button isLoading={isLoading} loadingText='Loading' onClick={tryLogin}>Login</Button>
+        </Flex>
+        
+        
+        <Flex justify={"center"}>
+          <Text >
+            <>Não tem uma conta? </>
+            <Link>Cadastre-se!</Link>
+          </Text>
+        </Flex>
+      </Card>
+    </Flex>
     );
 }
 
