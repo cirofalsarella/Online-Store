4dbbef660ea0e2a0b4764ea1d4299f3694b11900
Ciro Falsarella <cirofalsarella@gmail.com>
2023-05-31 23:43:10 -0300

    Feat: Profile Structural page

--- App/src/pages/user/profile/profile.js
+++ App/src/pages/user/profile/profile.js
@@ -1,6 +1,38 @@
+import { Button, Card, Flex, FormControl, FormLabel, Heading, Input, Textarea, Avatar } from "@chakra-ui/react"
+
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <Flex justify={"center"} align={"center"}>
+            <Card width={"70vw"} height={"70vh"} direction={"column"} align={"center"}>
+                <Heading margin={"10px"}>Seu Perfil</Heading>
+
+                <Flex justify={"space-around"}>
+                    <Flex width={"40vw"} height={"55vh"} direction={"column"} justify={"space-around"}>
+                        <FormControl>
+                            <FormLabel>UserName</FormLabel>
+                            <Input placeholder={"Nome"}></Input>
+                        </FormControl>
+                        <FormControl isDisabled>
+                            <FormLabel>E-mail</FormLabel>
+                            <Input placeholder={"E-mail"}></Input>
+                        </FormControl>
+                        <FormControl isDisabled>
+                            <FormLabel>CPF</FormLabel>
+                            <Input placeholder={"CPF"}></Input>
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Address</FormLabel>
+                            <Textarea placeholder={"Address"}></Textarea>
+                        </FormControl>
+                    </Flex>
+
+                    <Flex width={"20vw"} direction={"column"} justify={"space-between"} align={"center"}>
+                        <Avatar />
+                        <Button size={"lg"}>Atualizar</Button>
+                    </Flex>
+                </Flex>
+            </Card>
+        </Flex>
       );
   }
   
